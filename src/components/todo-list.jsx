c5e966491a2fa0df61f88e2ef81e173582c67dee
ibqn <evgen.ibqn@gmail.com>
2021-04-11 11:11:29 +0200

    basic look of todo item list

--- src/components/todo-list.jsx
+++ src/components/todo-list.jsx
@@ -1,19 +1,24 @@
 import styled from 'styled-components'
 import TodoItem from './todo-item'
 
-const MainSection = styled.section``
+const MainSection = styled.section`
+  z-index: 2;
+  border-top: 1px solid #e6e6e6;
+`
 
-const List = styled.ul``
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+`
 
 const TodoList = (props) => {
   const { list } = props
   return (
     <MainSection>
       <List>
-        {list &&
-          list.map((item) => (
-            <TodoItem key={item.id}>item.description</TodoItem>
-          ))}
+        {list?.map((item) => (
+          <TodoItem key={item.id}>item.description</TodoItem>
+        ))}
       </List>
     </MainSection>
   )
