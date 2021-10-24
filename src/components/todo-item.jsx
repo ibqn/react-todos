ae6905919d14829f8272c762adf1db257fcafc16
ibqn <evgen.ibqn@gmail.com>
2021-10-24 22:52:59 +0200

    add html for label

--- src/components/todo-item.jsx
+++ src/components/todo-item.jsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import styled from 'styled-components'
 
 const Li = styled.li`
@@ -11,12 +12,15 @@ const Li = styled.li`
 `
 
 const TodoItem = ({ item }) => {
-  const { editing, completed, title } = item
+  const { editing, completed, title, id } = item
+
+  const htmlFor = useMemo(() => `checkbox-${id}`, [id])
+
   return (
     <Li>
       <div>
-        <input type="checkbox" />
-        {title}
+        <input id={htmlFor} type="checkbox" checked={completed} />
+        <label htmlFor={htmlFor}>{title}</label>
       </div>
     </Li>
   )
